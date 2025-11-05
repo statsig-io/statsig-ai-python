471194d51a6f75fc1861fbea9dadfc2b2e26d100
“Kat <kat@statsig.com>
2025-11-04 16:16:31 -0800

    chore: rm otel methods

--- statsig_ai/__init__.py
+++ statsig_ai/__init__.py
@@ -1,15 +1,6 @@
-from statsig_python_core import Statsig, StatsigOptions, StatsigUser
 from .prompt import PromptEvaluationOptions, Prompt
 from .ai_eval_grade_data import AIEvalGradeData
 from .prompt_version import PromptVersion
-from .otel import (
-    initialize_tracing,
-    InitializeTracingOptions,
-    OtelSingleton,
-    StatsigSpanProcessor,
-    StatsigOTLPTraceExporter,
-    StatsigOTLPTraceExporterOptions,
-)
 from .statsig_ai import StatsigAI
 from .statsig_ai_base import StatsigCreateConfig, StatsigAttachConfig
 
@@ -23,11 +14,4 @@ __all__ = [
     "PromptEvaluationOptions",
     # Logging
     "AIEvalGradeData",
-    # Otel/Tracing
-    "initialize_tracing",
-    "InitializeTracingOptions",
-    "OtelSingleton",
-    "StatsigSpanProcessor",
-    "StatsigOTLPTraceExporterOptions",
-    "StatsigOTLPTraceExporter",
 ]
